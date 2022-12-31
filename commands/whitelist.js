2d32733cd004f78d9ab4b235600cfac742a697c2
Drikkx <88060062+Drikkx@users.noreply.github.com>
2022-12-31 13:48:29 +0100

    Update whitelist.js

--- commands/whitelist.js
+++ commands/whitelist.js
@@ -2,9 +2,4 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder().setName('whitelist').setDescription('Reply with Character Embed')
-        .addNumberOption((option) => option
-            .setName('id')
-            .setDescription('Input CharacterId')
-            .setRequired(true))
-            
 };
